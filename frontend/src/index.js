bffbfea73037c79f8da396022265378f2d295784
Aj-github-cmd <sahooamlanjyoti99@gmail.com>
2022-04-10 14:06:42 +0530

    built the landing page of squinote

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
 import App from './App';
-
+import {AuthProvider,AuthContext} from "./context/Authcontext"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <AuthProvider>
+  <App/>
+  </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
